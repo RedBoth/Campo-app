dda3087b05f2f9cb8496688c13e58a5a0a9ad86f
MauroKolman <mauro.kolman1255@gmail.com>
2025-10-15 18:05:42 -0300

    Cambios en configuracion de firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6i9Pr3ttkDQErJd8ZVvsvGveXPHi_3DI",
-  authDomain: "lotepro.firebaseapp.com",
-  projectId: "lotepro",
-  storageBucket: "lotepro.firebasestorage.app",
-  messagingSenderId: "72955952727",
-  appId: "1:72955952727:web:fcad0499417ca2ebb06d12"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Inicializar Firebase
