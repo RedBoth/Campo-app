33bef52e2a568f5b9e5ad9a493f13b6d23e4850d
MauroKolman <mauro.kolman1255@gmail.com>
2025-11-19 20:44:41 -0300

    Fix redireccionamiento al cerrar sesion

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
 
   const handleLogout = async () => {
     await logout();
-    window.location.href = "/login";
+    window.location.href = "/";
   };
 
   return (
